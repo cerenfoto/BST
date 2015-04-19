523f86c64258079c4cc61773978c4951262de29c
cerenfoto <cerenfoto@gmail.com>
2015-04-19 21:19:24 +0300

    BinarySearchTree
    Random olarak ağaç oluşturma ve random numaralar arama

---
+++ src/bst/BST.java
@@ -0,0 +1,31 @@
+package bst;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class BST {
+
+    public static void main(String[] args) {
+        Random rnd = new Random();
+
+        BinaryTree tree = new BinaryTree();
+        
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Kaç Node lu ağaç oluşturacaksınız?");
+        int node_sayisi=scan.nextInt();
+        System.out.println("Kaç tane arama yapmak istersiniz?");
+        int arama_sayisi=scan.nextInt();
+        while (tree.countLeaves(tree.root) != node_sayisi) {
+            tree.AddNode(rnd.nextInt(2147483647));
+        }
+        int gezilen=0;
+        for(int i=0;i<arama_sayisi;i++){
+            gezilen=gezilen+tree.sayarakArama(rnd.nextInt(2147483647));
+        }
+        System.out.println("GEzinti Ortalaması:");
+        System.out.println(gezilen/arama_sayisi);
+
+//        tree.PreOrderTraverseTree(tree.root);
+
+    }
+}
